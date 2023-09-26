3bce762a12543cf31485e5ae3ba5fab4aef8cdd4
Роман <savinr95@gmail.com>
2023-09-26 20:42:09 +0300

    feat: "добавил поверку юзера перед добавлением и обновлением"

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -5,8 +5,12 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.dto.UserDto;
 import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.service.RoleService;
 import ru.kata.spring.boot_security.demo.service.UserService;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/admin")
@@ -14,37 +18,48 @@ import ru.kata.spring.boot_security.demo.service.UserService;
 @RequiredArgsConstructor
 public class AdminController {
     private final UserService userService;
+    private final RoleService roleService;
 
     @GetMapping
-    public String listUsers(ModelMap model) {
+    public String listUsers(ModelMap model, Principal principal) {
+        model.addAttribute("user", userService.getUserByPrincipal(principal));
         model.addAttribute("users", userService.listUsers());
+        model.addAttribute("roles", roleService.getRoles());
         return "users";
     }
 
     @PostMapping("/add")
-    public String add(ModelMap model, User user) {
-        userService.add(user);
+    public String add(ModelMap model, UserDto userDto) {
+        userService.existByEmail(userDto.getEmail());
+        userService.add(userDto);
         model.addAttribute("users", userService.listUsers());
+        model.addAttribute("roles", roleService.getRoles());
         return "users";
     }
 
     @GetMapping("/update/{id}")
     public String update(ModelMap model, @PathVariable Long id) {
+        userService.existById(id);
         model.addAttribute("user", userService.findById(id));
+        model.addAttribute("roles", roleService.getRoles());
         return "update-user";
     }
 
     @PatchMapping("/update")
-    public String update(ModelMap model, User user) {
-        userService.update(user);
+    public String update(ModelMap model, User user, @RequestParam String role) {
+        userService.existById(user.getId());
+        userService.update(user, role);
         model.addAttribute("users", userService.listUsers());
+        model.addAttribute("roles", roleService.getRoles());
         return "users";
     }
 
     @DeleteMapping("/delete/{id}")
     public String delete(ModelMap model, @PathVariable Long id) {
+        userService.existById(id);
         userService.remove(id);
         model.addAttribute("users", userService.listUsers());
+        model.addAttribute("roles", roleService.getRoles());
         return "users";
     }
 }
