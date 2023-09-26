eed3cb888116faaef1671baa707dc4896b93c558
Роман <savinr95@gmail.com>
2023-09-26 20:46:10 +0300

    feat: "добавил методы помошники"

--- src/main/java/ru/kata/spring/boot_security/demo/model/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/User.java
@@ -1,12 +1,8 @@
 package ru.kata.spring.boot_security.demo.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import ru.kata.spring.boot_security.demo.model.enums.Role;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -28,17 +24,50 @@ public class User implements UserDetails {
     private String firstName;
     @Column(name = "last_name")
     private String lastName;
-    @Column(name = "email")
+    @Column(unique = true, nullable = false, name = "email")
     private String email;
+    private Integer age;
     private boolean active;
     @Column(length = 1000)
     private String password;
-    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
-    @CollectionTable(name = "user_role",
-            joinColumns = @JoinColumn(name = "user_id"))
-    @Enumerated(EnumType.STRING)
+    @ManyToMany(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "User_Roles",
+            joinColumns = {@JoinColumn(name = "User_id")},
+            inverseJoinColumns = {@JoinColumn(name = "Role_id")}
+    )
+    @ToString.Exclude
     private Set<Role> roles = new HashSet<>();
     private LocalDateTime dateOfCreated;
+
+    public User(String firstName, String lastName, String email, String password, Integer age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.age = age;
+        this.password = password;
+    }
+
+    public User(String firstName, String lastName, String email, String password, Integer age, Role role) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.age = age;
+        this.password = password;
+        this.roles.add(role);
+    }
+
+    public String getRole() {
+        return roles.stream().findFirst().get().getName();
+    }
+
+    public void addRole(Role roleUser) {
+        roles.add(roleUser);
+    }
+
+    public void deleteRole(Role roleUser) {
+        roles.remove(roleUser);
+    }
 
     @PrePersist
     private void init() {
