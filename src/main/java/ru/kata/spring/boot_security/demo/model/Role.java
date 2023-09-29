a760c96865a85289285f2cd6b2b8fe661381b6e6
Роман <savinr95@gmail.com>
2023-09-29 14:06:43 +0300

    fix: "убрал связь с юзером"

--- src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
@@ -1,28 +1,23 @@
 package ru.kata.spring.boot_security.demo.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 
 import javax.persistence.*;
-import java.util.Set;
 
 @Getter
 @Setter
 @Entity
+@EqualsAndHashCode
 @Table(name = "roles")
 @AllArgsConstructor
 @NoArgsConstructor
 public class Role implements GrantedAuthority {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
     @Column(name = "name", nullable = false, unique = true)
     private String name;
-    @ManyToMany(mappedBy = "roles")
-    Set<User> users;
 
     public Role(String name) {
         this.name = name;
